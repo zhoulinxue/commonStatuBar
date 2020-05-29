f0eb8145e930d435a9b83801e2739540d9e295c5
zhouxue@lobochat.cn <zlx871026>
2020-05-29 17:19:17 +0800

    测试 fragment dialog  keyboard

--- app/src/main/java/com/zhx/commonapi/common/statubar/demo/MainActivity.kt
+++ app/src/main/java/com/zhx/commonapi/common/statubar/demo/MainActivity.kt
@@ -2,13 +2,23 @@ package com.zhx.commonapi.common.statubar.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
 import com.zhx.commonapi.common.statubar.CommonStatusBar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tanchuang_btn.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                var fragment:DialogTestFragment= DialogTestFragment()
+                fragment.show(supportFragmentManager,"test")
+            }
+        })
+//        supportFragmentManager.beginTransaction().replace(R.id.container,TestFragment()).commit()
     }
 
     override fun onResume() {
